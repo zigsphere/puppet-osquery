3f8cc9d701c873501d6b8219a4b6badefff4153b
Jacob McCoy Wade <jacobmw@uber.com>
2016-09-19 12:23:46 -0700

    Updating scope of settings variable

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,15 +1,11 @@
 # class osquery::config - manage json config in /etc/osquery
-class osquery::config (
-
-  $settings = $::osquery::params::settings,
-
-){
+class osquery::config {
 
   file { $::osquery::params::config:
     ensure  => present,
     owner   => root,
     group   => root,
-    content => sorted_json($::settings), # array to JSON lib
+    content => sorted_json($::osquery::settings), # array to JSON lib
     require => Package[$::osquery::params::package_name],
     notify  => Service[$::osquery::params::service_name],
   }
